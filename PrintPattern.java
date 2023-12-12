68a6242deef513af2150b234f8ae78e1eb09cfe4
veerapuraju <116946808+veerapuraju@users.noreply.github.com>
2023-12-12 15:13:20 +0530

    PrintPattern.java

---
+++ PrintPattern.java
@@ -0,0 +1,20 @@
+//printing pattern
+import java.util.Scanner;
+
+public class PrintPattern
+{
+	public static void main(String args[])
+	{
+		Scanner reader = new Scanner(System.in);
+		System.out.print("Enter the Iteration of patterns: ");
+		int n = reader.nextInt();
+		for(int i = 1; i<=n; i++)
+		{
+			for(int j=1; j<=i; j++)
+			{
+				System.out.print(j + " ");
+			}
+			System.out.print("\n");
+		}
+	}
+}
